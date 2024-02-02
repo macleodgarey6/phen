735f4dfc7ce31f4ae1073b3920aa3ad9d8a5c564
macleodgarey6 <158565136+macleodgarey6@users.noreply.github.com>
2024-02-02 11:35:22 +0000

    Update redirect.js

--- netlify/functions/redirect.js
+++ netlify/functions/redirect.js
@@ -1,7 +1,7 @@
 // netlify/functions/redirect.js
 exports.handler = async (event, context) => {
     const currentDomain = event.queryStringParameters.sub1;
-    const affiliateUrl = `https://www.eb9futrk.com/SRDRLT8/GF7LN2R/?sub1=${currentDomain}`;
+    const affiliateUrl = `https://www.eb9futrk.com/SRDRLT8/GDZN8CQ/?sub1=${currentDomain}`;
 
     return {
         statusCode: 200,
